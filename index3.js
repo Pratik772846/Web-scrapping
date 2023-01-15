e1af5c0ef4c1fdd7aa26be57efd4936e1ac9e688
Pratik <singhpratik015@gmail.com>
2023-01-16 00:32:04 +0530

    routing-added

--- index3.js
+++ index3.js
@@ -1,37 +1,50 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const express = require('express');
 
-axios.get('https://www.sluurpy.in/jaipur/restaurants')
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.json("Web scrapper");
+})
+app.get('/restaurants',(req,res)=>{
+
+    axios.get('https://www.sluurpy.in/jaipur/restaurants')
     .then(
     async (response) => {
         const html = response.data;
         // console.log(html);
+        const details = [];
         const $ = cheerio.load(html);
 
         // restaurant name correct
-        // $('.box-ristorante-titolo').each((i, el) => {
-        //     const item = $(el).find('strong').text();
-        //     console.log(item);
-        // });
+        $('.box-ristorante-titolo').each((i, el) => {
+            const item = $(el).find('strong').text();
+            // console.log(item);
+            details.push(name = item);
+        });
 
         // restaurant review out of 100 correct
         // $('.box-ristorante-voto').each((i, el) => {
         //     const item = $(el).find('strong').text();
-        //     console.log(item);
+        //     // console.log(item);
+        //     details.push({item});
         // });
 
         // restaurant review count correct 
         // if we use this then no need to use above one
-        // $('.box-ristorante-voto').each((i, el) => {
-        //     const item = $(el).text();
-        //     console.log(item);
-        // });
+        $('.box-ristorante-voto').each((i, el) => {
+            const item = $(el).text();
+            // console.log(item);
+            details.push({item});
+        });
 
         // // restaurant image link  correct
-        // $('.box-ristorante-immagine').each((i,el)=>{
-        //     const item = $(el).attr('data-src');
-        //     console.log(item);
-        // })
+        $('.box-ristorante-immagine').each((i,el)=>{
+            const item = $(el).attr('data-src');
+            // console.log(item);
+            details.push({item});
+        })
 
 
         
@@ -47,8 +60,15 @@ axios.get('https://www.sluurpy.in/jaipur/restaurants')
         //     const item = $(el).find('box-ristorante-tag').text();
         //     console.log(item);
         // })
-
+        
+        console.log(details);
+        res.json(details);
     })  
         .catch((error) => {
         console.log(error);
     });
+});
+
+app.listen(5000,()=>{
+    console.log("server is running on port 5000");
+})
